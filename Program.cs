808d104d8e152d8c62609687204bfde5a5483c6e
pavlo <pavlo@DESKTOP-SM098C1>
2021-12-31 11:30:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Speech.Synthesis;
+
+namespace Twister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpeechSynthesizer speechSynth = new SpeechSynthesizer(); // создаём объект
+
+            speechSynth.Volume = 100; // устанавливаем уровень звука
+            speechSynth.Speak("Right foot on green"); // озвучиваем переданный текст
+
+          
+        }
+    }
+}
---
+++ Twister.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twister
+{
+    public class Twister
+    {
+        Random rand;
+        string []_bodyParts;
+        string[] _colors;
+        public Twister()
+        {
+            rand = new Random();
+            _bodyParts = new string[] { "Right foot", "Left foot", "Left hand", "Right hand" };
+            _colors = new string[] { "Green", "Red", "Blue", "Yellow" };
+        }
+        public string GetMove()
+        {
+            int index = rand.Next(4);
+            return $"Put your {_bodyParts[index]} on {_colors[index]}"; 
+        }
+    }
+}
